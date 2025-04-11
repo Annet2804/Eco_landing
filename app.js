32e9243546ad52d875d499acb85e02ef999325f6
Anna <ledy-zaharova-03@ukr.net>
2025-04-11 16:50:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+$(function () {
+
+    // fixed header
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH =intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH)
+
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH);
+    });
+
+
+        function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+        }
+
+   
+
+    
+
+    // smoothScroll
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        nav.removeClass("show");
+
+
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 50
+        }, 700);
+    
+    });
+
+
+    // nav toggle
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    $("#navToggle").on("click", function (event){
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+    })
+
+    // reviews
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true, 
+        arrows: false,
+        dots: true
+
+});
+
+
+});
